1798394b78405674d70a903c44434dffaa6e9f13
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2019-01-08 14:33:59 -0500

    First Checkin

---
+++ src/main/java/com/zipcode/localh2server/LocalH2ServerApplication.java
@@ -0,0 +1,28 @@
+package com.zipcode.localh2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class LocalH2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalH2ServerApplication.class, args);
+    }
+
+    /**
+     * Start internal H2 server so we can query the DB from IDE
+     *
+     * @return H2 Server instance
+     * @throws SQLException
+     */
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+}
+
